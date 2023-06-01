3e176ea47b55ca187db1b4522fa68f1d0d8b1613
algofoogle <anton@foogle.com>
2023-06-01 16:41:13 +0200

    Fixing some typos

--- src/top.v
+++ src/top.v
@@ -1,3 +1,6 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
 module tt_um_algofoogle_solo_squash(
     // 8 dedicated user inputs:
     input   wire [7:0]  ui_in,
@@ -8,7 +11,7 @@ module tt_um_algofoogle_solo_squash(
     // 8 bidirectional user IOs:
     output  wire [7:0]  uio_in,     // Input path.
     output  wire [7:0]  uio_out,    // Output path.
-    output  wire [7:0]  ui_oe,      // Enable path (active high: 0=input, 1=output)
+    output  wire [7:0]  uio_oe,      // Enable path (active high: 0=input, 1=output)
 
     // Control:
     input   wire        ena,        // Can be ignored for now.
@@ -20,11 +23,10 @@ module tt_um_algofoogle_solo_squash(
     assign ui_oe = 8'b0;
 
     // For test purposes, use unused outputs to loop back a couple of things:
-    // assign ui_out[5] = 0;
-    assign ui_out[6] = clk;
-    assign ui_out[7] = ~ui_in[4]; // Unused input, just inverted for testing.
+    assign uo_out[6] = clk;
+    assign uo_out[7] = ~ui_in[4]; // Unused input, just inverted for testing.
 
-    solo_squash solo_squash(
+    solo_squash game(
         // --- Inputs ---
         .clk        (clk),
         .reset      (~rst_n),       // Active HIGH reset needed here.
@@ -35,12 +37,12 @@ module tt_um_algofoogle_solo_squash(
         .up_key_n   (~ui_in[3]),
 
         // --- Outputs ---
-        .red        (ui_out[0]),
-        .green      (ui_out[1]),
-        .blue       (ui_out[2]),
-        .hsync      (ui_out[3]),
-        .vsync      (ui_out[4]),
-        .speaker    (ui_out[5])
+        .red        (uo_out[0]),
+        .green      (uo_out[1]),
+        .blue       (uo_out[2]),
+        .hsync      (uo_out[3]),
+        .vsync      (uo_out[4]),
+        .speaker    (uo_out[5])
     );
 
 endmodule

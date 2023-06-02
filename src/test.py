d81f35263e1ce6b5d0f96ea97dc4cbdbba8b32ad
algofoogle <anton@foogle.com>
2023-06-02 18:14:45 +0200

    Basic first lines and first frame test now works adequately

--- src/test.py
+++ src/test.py
@@ -128,81 +128,99 @@ async def test_frame0(dut):
     # Meanwhile note that HSYNC and VSYNC are NOT registered (because I expect some skew in their
     # timing is fine).
 
-    print("--- TEST DEBUG: Testing first line...")
-    # First, compensate for 1-clock delay due to registered RGB outputs:
+    # Check that the first 2 lines are fully yellow (other than proper HBLANK and HSYNC):
+    for v in range(2):
+        print(f"--- TEST DEBUG: Testing line {v}...")
+        # Visible part of line:
+        for h in range(640):
+            if h>0:    # Don't check at h==0, because RGB lags by 1 clock.
+                # Expect yellow:
+                assert dut.red.value    == 1
+                assert dut.green.value  == 1
+                assert dut.blue.value   == 0
+            # Expect HSYNC and VSYNC are NOT asserted:
+            assert dut.hsync.value  == 1
+            assert dut.vsync.value  == 1
+            # Advance to the next clock:
+            await ClockCycles(dut.clk, 1)
+        # HFRONT porch:
+        for h in range(HF):
+            if h==0:    # At h==0, RGB are still lagging because they're registered while HSYNC is not.
+                # Expect yellow for 1 more pixel:
+                assert dut.red.value    == 1
+                assert dut.green.value  == 1
+                assert dut.blue.value   == 0
+            else:
+                # Expect black:
+                assert dut.red.value    == 0
+                assert dut.green.value  == 0
+                assert dut.blue.value   == 0
+            # Expect HSYNC and VSYNC are NOT asserted:
+            assert dut.hsync.value  == 1
+            assert dut.vsync.value  == 1
+            # Advance to the next clock:
+            await ClockCycles(dut.clk, 1)
+        # HSYNC pulse:
+        for h in range(HS):
+            # Expect black:
+            assert dut.red.value    == 0
+            assert dut.green.value  == 0
+            assert dut.blue.value   == 0
+            # Expect HSYNC is now asserted, but VSYNC is not:
+            assert dut.hsync.value  == 0
+            assert dut.vsync.value  == 1
+            # Advance to the next clock:
+            await ClockCycles(dut.clk, 1)
+        # HBACK porch:
+        for h in range(HB):
+            # Expect black:
+            assert dut.red.value    == 0
+            assert dut.green.value  == 0
+            assert dut.blue.value   == 0
+            # Expect HSYNC and VSYNC are NOT asserted:
+            assert dut.hsync.value  == 1
+            assert dut.vsync.value  == 1
+            # Advance to the next clock:
+            await ClockCycles(dut.clk, 1)
+    # Now we get a little lazy... the NEXT two lines should have some green and yellow pixels (only)
+    # so count how many of each:
+    yellow_count = 0
+    green_count = 0
+    for v in range(2):
+        print(f"--- TEST DEBUG: Testing line {2+v}...")
+        for h in range(640):
+            if h>0:
+                assert dut.blue.value == 0
+                assert dut.green.value == 1
+                if dut.red.value:
+                    yellow_count += 1
+                else:
+                    green_count += 1
+            await ClockCycles(dut.clk, 1)
+        for h in range(HF+HS+HB):
+            await ClockCycles(dut.clk, 1)
+    # Horizontal "bricks" in the upper wall are each 32 pixels wide, so there are 640/32 = 20 of them.
+    # The yellow "mortar" between any pair of them is 4 pixels wide, while the outer-most mortar on
+    # each side is 2 pixels wide. Thus, 80 yellow pixels per line in total, but to keep things simple
+    # we only end up counting 79 per line, above, or 158 total per 2 lines.
+    assert yellow_count == 158
+    # Green pixels should be the difference of 1280-160, i.e. 1120:
+    assert green_count == 1120
+
+    # Now render the remaining lines for this frame, just to get a full VCD:
+    for v in range(480+VF+VS+VB-4):
+        print(f"--- TEST DEBUG: Skimming line {4+v}...")
+        for h in range(800):
+            await ClockCycles(dut.clk, 1)
+
+    print("--- TEST DEBUG: First frame completed")
+    # Verify we're in a black region...
+    assert dut.red.value    == 0
+    assert dut.green.value  == 0
+    assert dut.blue.value   == 0
+    # Wait 1 more clock...
     await ClockCycles(dut.clk, 1)
-    # Visible part of line:
-    for h in range(640):
-        # Expect yellow:
-        assert dut.red.value    == 1
-        assert dut.green.value  == 1
-        assert dut.blue.value   == 0
-        # Expect HSYNC and VSYNC are NOT asserted:
-        assert dut.hsync.value  == 1
-        assert dut.vsync.value  == 1
-        # Advance to the next clock:
-        await ClockCycles(dut.clk, 1)
-    # HFRONT porch:
-    for h in range(HF-1):  #NOTE: HF-1, because following it is HSYNC which will APPEAR to be 1 clock early, because it is not registered.
-        # Expect black:
-        assert dut.red.value    == 0
-        assert dut.green.value  == 0
-        assert dut.blue.value   == 0
-        # Expect HSYNC and VSYNC are NOT asserted:
-        assert dut.hsync.value  == 1
-        assert dut.vsync.value  == 1
-        # Advance to the next clock:
-        await ClockCycles(dut.clk, 1)
-    # HSYNC pulse:
-    for h in range(HS):
-        # Expect black:
-        assert dut.red.value    == 0
-        assert dut.green.value  == 0
-        assert dut.blue.value   == 0
-        # Expect HSYNC is now asserted, but VSYNC is not:
-        assert dut.hsync.value  == 0
-        assert dut.vsync.value  == 1
-        # Advance to the next clock:
-        await ClockCycles(dut.clk, 1)
-    # HBACK porch:
-    for h in range(HB):
-        # Expect black:
-        assert dut.red.value    == 0
-        assert dut.green.value  == 0
-        assert dut.blue.value   == 0
-        # Expect HSYNC and VSYNC are NOT asserted:
-        assert dut.hsync.value  == 1
-        assert dut.vsync.value  == 1
-        # Advance to the next clock:
-        await ClockCycles(dut.clk, 1)
-
-    # # The first 2 lines of EVERY frame should be yellow, i.e. R&G both asserted for 640 clocks,
-    # # then nothing for the duration of the horizontal front porch, then hsync asserted, then
-    # # horizontal back porch, and then it should repeat again.
-    # # First, though, compensate for 1-clock delay due to registered RGB outputs:
-    # await ClockCycles(dut.clk, 1)
-    # for h in range(640):
-    #     # Expect yellow:
-    #     assert dut.red.value    == 1
-    #     assert dut.green.value  == 1
-    #     assert dut.blue.value   == 0
-    #     # Expect HSYNC and VSYNC are NOT asserted:
-    #     assert dut.hsync.value  == 1
-    #     assert dut.vsync.value  == 1
-    #     # Advance to the next clock:
-    #     await ClockCycles(dut.clk, 1)
-    # # HBLANK area at the end of the line:
-    # for h in range(HF+HS+HB):
-    #     assert dut.red.value    == 0, f"Red channel shouldn't be asserted at h:{640+h}"
-    #     assert dut.green.value  == 0, f"Green channel shouldn't be asserted at h:{640+h}"
-    #     assert dut.blue.value   == 0, f"Blue channel shouldn't be asserted at h:{640+h}"
-    #     if h<=HF or h>=HF+HS:
-    #         assert dut.hsync.value == 1
-    #     else:
-    #         assert dut.hsynv.value == 0
-
-    # # Now we should be able to await 420,001 clock cycles and prove
-    # # that a full frame (plus 1 clock) completes:
-    # await ClockCycles(dut.clk, 420_001)
-    # print("--- TEST DEBUG: First frame completed")
-
+    # ...and then we should be able to verify we've got a yellow pixel again:
+    assert dut.red.value    == 1
+    assert dut.green.value  == 1
+    assert dut.blue.value   == 0
